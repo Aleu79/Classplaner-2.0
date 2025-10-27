190506270019cc1d1f23a807b9888ba1eec1bb89
Julian, Mayola <mayola@mayola.net.ar>
2025-10-27 01:10:27 -0300

    fixes in merge

--- internal/model/task.go
+++ internal/model/task.go
@@ -9,26 +9,11 @@ const (
 )
 
 type Tasks struct {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> refs/remotes/origin/routing
-	ID          int     `json:"id_task" sql:"primary_key;auto_increment"`
-	Clase       Classes `json:"id_class" sql:"foreign_key:id;references:id"`
-	Titulo      string  `json:"title" sql:"type:VARCHAR(200)"`
-	Description string  `json:"description" sql:"type:VARCHAR(1000)"`
-	Creado      string  `json:"created_on" sql:"type:timestamp"`
-	Limite      string  `json:"deliver_until" sql:"type:timestamp"`
-<<<<<<< HEAD
-=======
-	ID          int       `json:"id_task"`
-	Clase       int       `json:"id_class"`
-	Titulo      string    `json:"title"`
-	Estado      TaskState `json:"estado"`
-	Description string    `json:"description"`
-	Creado      string    `json:"created_on"`
-	Limite      string    `json:"deliver_until"`
->>>>>>> fe019da
-=======
->>>>>>> refs/remotes/origin/routing
+	ID          int       `json:"id_task" sql:"primary_key;auto_increment"`
+	Clase       Classes   `json:"id_class" sql:"foreign_key:id;references:id"`
+	Titulo      string    `json:"title" sql:"type:VARCHAR(200)"`
+	Description string    `json:"description" sql:"type:VARCHAR(1000)"`
+	Creado      string    `json:"created_on" sql:"type:timestamp"`
+	Limite      string    `json:"deliver_until" sql:"type:timestamp"`
+	Estado      TaskState `json:"estado" sql:"type:VARCHAR(100)"`
 }
