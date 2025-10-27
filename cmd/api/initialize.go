01b96f7d8aeaedb0fd17379bef5647ef3d477542
Aleu79 <aleeaguero79@gmail.com>
2025-10-26 22:03:29 -0300

    feat,refactor(logger,service):Add the logger to the project and change user_service to use the logger

--- cmd/api/initialize.go
+++ cmd/api/initialize.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"context"
+	"log"
 	"os"
 	"time"
 
 	"classplanner/internal/infrastructure/database"
+	"classplanner/internal/infrastructure/logger"
 	"classplanner/internal/middleware"
 	"classplanner/internal/repository"
 	"classplanner/internal/service"
@@ -12,6 +15,7 @@ import (
 	"classplanner/pkg/utils"
 
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 // AppDependencies holds all the dependencies to be injected into the handlers
@@ -19,19 +23,18 @@ type AppDependencies struct {
 	UserHandler *users.UserHandler
 	DB          *database.DatabaseInstance
 	App         *fiber.App
+	Logger      *logger.Logger
 }
 
 // Initialize loads configuration, middlewares, database, repositories, and services
 func Initialize() *AppDependencies {
 	utils.LoadEnv()
 
-	// Create a new Fiber app
 	app := fiber.New(fiber.Config{
 		ServerHeader: os.Getenv("SERVER_HEADER"),
 		AppName:      os.Getenv("APP_NAME"),
 	})
 
-	// Connect to the database
 	dbInstance := database.Connect()
 
 	// Register middlewares
@@ -48,14 +51,25 @@ func Initialize() *AppDependencies {
 		MaxAge:        36000,
 	})
 
+	// Initialize logger
+	logFile := "./logs/app.log"
+	l, err := logger.NewLogger(logFile, zap.DebugLevel)
+	if err != nil {
+		log.Fatalf("no se pudo inicializar logger: %v", err)
+	}
+
 	// Initialize repositories and services
 	userRepo := repository.NewUserRepository(dbInstance.DB)
-	userService := service.NewUserService(userRepo)
+	userService := service.NewUserService(userRepo, l)
 	userHandler := users.NewUserHandler(userService)
+
+	ctx := context.Background()
+	l.Info(logger.EnsureContext(ctx), "Aplicación iniciada")
 
 	return &AppDependencies{
 		UserHandler: userHandler,
 		DB:          dbInstance,
 		App:         app,
+		Logger:      l,
 	}
 }
--- internal/transport/users/GetAll_handler.go
+++ internal/transport/users/GetAll_handler.go
@@ -1,12 +1,15 @@
 package users
 
 import (
+	"context"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 // Obtener todos los usuarios
 func (h *UserHandler) GetAll(c *fiber.Ctx) error {
-	users, err := h.service.GetAll()
+	ctx := context.Background()
+	users, err := h.service.GetAll(ctx)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
